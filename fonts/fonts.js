34b89e5317199c0364dc8707ccc4c4844d691324
Saksham Bhatt <sakshambhatt33@gmail.com>
2022-12-09 08:01:58 +0530

    display swap to apply font when loading

--- fonts/fonts.js
+++ fonts/fonts.js
@@ -4,12 +4,16 @@ const cursive = Cedarville_Cursive({
   weight: "400",
   style: ["normal"],
   subsets: ["latin"],
+  fallback: ["system-ui"],
+  display: "swap",
 });
 
 const eater = Eater({
   weight: "400",
   style: ["normal"],
   subsets: ["latin"],
+  fallback: ["system-ui"],
+  display: "swap",
 });
 
 export { cursive, eater };
